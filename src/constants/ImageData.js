c46bfb2b40c23ed9e3277db8fee8f60ac9274f3c
Sakshi Goyal <sakgo013@in.ibm.com>
2022-10-03 14:52:26 +0530

    update homepage with content

---
+++ src/constants/ImageData.js
@@ -0,0 +1,48 @@
+export const imageData = [
+    {
+        id: 1,
+        img: process.env.PUBLIC_URL + "/solaytic.jpg",
+        title: 'Solyatic'
+    },
+    {
+        id: 2,
+        img: process.env.PUBLIC_URL + "/kanba.jpg",
+        title: 'Kanba'
+    },
+    {
+        id: 3,
+        img: process.env.PUBLIC_URL + "/lighting.jpg",
+        title: 'LightAI'
+    },
+    {
+        id: 4,
+        img: process.env.PUBLIC_URL + "/ztos.jpg",
+        title: 'Ztos'
+    },
+    {
+        id: 5,
+        img: process.env.PUBLIC_URL + "/kanba.jpg",
+        title: 'Kanba'
+    },
+    {
+        id: 6,
+        img: process.env.PUBLIC_URL + "/goldline.jpg",
+        title: 'Goldline'
+    },
+    {
+        id: 7,
+        img: process.env.PUBLIC_URL + "/ideaa.jpg",
+        title: 'Ideaa'
+    },
+    {
+        id: 8,
+        img: process.env.PUBLIC_URL + "/liva.jpg",
+        title: 'Liva'
+    },
+    {
+        id: 9,
+        img: process.env.PUBLIC_URL + "/velocity-9.jpg",
+        title: 'Velocity'
+    },
+    
+];
